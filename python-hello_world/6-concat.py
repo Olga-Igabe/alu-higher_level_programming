02179faa1e8ef0e9204c2e63aa45719ce6fd8e93
Olga-Igabe <o.igabe@alustudent.com>
2026-06-25 00:15:57 -0700

    Python Hello World tasks completed

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Welcome"
+str2 = "Holberton School!"
+print(f"{str1} {str2}")
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[0:6] + str[9:12] + str[39:40])
