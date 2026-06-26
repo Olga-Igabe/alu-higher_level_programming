cafa9d5fbcc8de00b2772b8423a21d14786603e4
Olga-Igabe <o.igabe@alustudent.com>
2026-06-26 04:39:20 -0700

    fixing the file

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[0:26] + word[7:] )
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[0:6])
