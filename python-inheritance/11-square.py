9b76e80ebf07f8f6663779b7b8defcccb424216d
Olga-Igabe <o.igabe@alustudent.com>
2026-07-21 09:36:04 +0200

    Refactor Square class documentation and methods

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Module that defines a Square class with a custom string
-representation."""
+"""Module that defines a Square class with string representation."""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square, inheriting from Rectangle."""
+    """Square class inheriting from Rectangle."""
 
     def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size (int): the length of each side of the square.
-        """
+        """Initialize square with size."""
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the square area."""
+        return self.__size ** 2
 
     def __str__(self):
-        """Return the string representation of the square."""
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                        self._Rectangle__height)
+        """Return square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
