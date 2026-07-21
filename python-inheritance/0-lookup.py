2633345722d67d8969d9b08c23d4cd1f6a247acf
Olga-Igabe <o.igabe@alustudent.com>
2026-07-21 00:14:24 -0700

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that returns the list of attributes and methods of an object."""
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
